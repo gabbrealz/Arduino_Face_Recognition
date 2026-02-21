b0396c5a776f55788f8fcdeefadee89f4084a2cf
unknown <christianagot11@gmail.com>
2026-02-21 18:46:31 +0800

    added unique constraint to student number

--- db-init/schema.sql
+++ db-init/schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE public.students (
 
     CONSTRAINT students_pkey PRIMARY KEY (id),
     CONSTRAINT student_number_pattern CHECK (student_number ~ '^\d{4}-\d{3}-\d{3}$'),
+    CONSTRAINT unique_student_number UNIQUE (student_number),
     CONSTRAINT unique_email UNIQUE (student_email),
     CONSTRAINT email_pattern CHECK (student_email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
 );
