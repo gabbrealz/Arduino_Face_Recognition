356e55f209f09e4d2dbdd5fa570963dcd7e2ff15
unknown <christianagot11@gmail.com>
2026-02-21 17:54:08 +0800

    specified columns in select queries. modified attendance_logs query

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,6 +1,6 @@
 import os
 import psycopg
-import datetime
+from datetime import datetime
 
 DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
 DB_PORT = os.getenv("POSTGRES_PORT", 5432)
@@ -11,28 +11,30 @@ DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 class DB:
     conn = psycopg.connect(
         host=DB_HOST,
-        port=DB_PORT
+        port=DB_PORT,
         dbname=DB_NAME,
         user=DB_USER,
-        password=DB_PASSWORD,
+        password=DB_PASSWORD
     )
 
     
     @staticmethod
-    def get_matching_face(embedding):
+    def get_matching_face(embedding, threshold):
         with DB.conn.cursor() as cur:
-            return cur.execute("SELECT get_matching_face(%s)", (embedding)).fetchone()
+            return cur.execute("SELECT get_matching_face(%s, %s)", (embedding, threshold)).fetchone()
 
 
     @staticmethod
     def students():
         with DB.conn.cursor() as cur:
-            return cur.execute("SELECT * FROM public.students").fetchall()
+            return cur.execute("SELECT id, student_number, full_name, student_email FROM public.students").fetchall()
 
     @staticmethod
     def student(student_id):
         with DB.conn.cursor() as cur:
-            return cur.execute("SELECT * FROM public.students WHERE id = %s LIMIT 1", (student_id)).fetchone()
+            return cur.execute(
+                "SELECT id, student_number, full_name, student_email FROM public.students WHERE id = %s LIMIT 1", 
+                (student_id)).fetchone()
 
     @staticmethod
     def insert_student(student_number, full_name, student_email):
@@ -45,16 +47,18 @@ class DB:
     @staticmethod
     def attendance_logs():
         with DB.conn.cursor() as cur:
-            return cur.execute("SELECT * FROM public.attendance_logs").fetchall()
+            return cur.execute("""
+                SELECT l.created_at, s.student_number, s.full_name, s.student_email
+                FROM public.attendance_logs l
+                JOIN public.students s ON l.student_id = s.id
+            """).fetchall()
         
     @staticmethod
     def insert_attendance_log(student_id):
-        time_now = datetime.datetime.now()
-
         with DB.conn.cursor() as cur:
             cur.execute(
                 "INSERT INTO public.attendance_logs (student_id, created_at) VALUES (%s, %s)",
-                (student_id, time_now))
+                (student_id, datetime.now()))
 
 
     @staticmethod
