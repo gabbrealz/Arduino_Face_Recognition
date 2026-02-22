0eb33e1b2ed51f324b7a60571151a8826f2c39c0
unknown <christianagot11@gmail.com>
2026-02-22 13:28:21 +0800

    separated table definitions and functions into separate sql files
    added new register face function

--- db-init/001_schema.sql
+++ db-init/001_schema.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION vector;
 
+
 CREATE TABLE public.students (
     id BIGSERIAL,
     student_number CHAR(12) NOT NULL,
@@ -12,6 +13,7 @@ CREATE TABLE public.students (
     CONSTRAINT unique_email UNIQUE (student_email),
     CONSTRAINT email_pattern CHECK (student_email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
 );
+
 
 CREATE TABLE public.face_embeddings (
     id BIGSERIAL,
@@ -27,6 +29,7 @@ CREATE TABLE public.face_embeddings (
 CREATE INDEX ON public.face_embeddings USING ivfflat (embedding vector_cosine_ops);
 ANALYZE public.face_embeddings;
 
+
 CREATE TABLE public.attendance_logs (
     id BIGSERIAL,
     student_id BIGINT NOT NULL,
@@ -38,35 +41,3 @@ CREATE TABLE public.attendance_logs (
     ON UPDATE CASCADE
     ON DELETE NO ACTION
 );
-
-
-CREATE OR REPLACE FUNCTION log_attendance_for_face(p_embedding VECTOR(512), p_threshold REAL)
-RETURNS TABLE(
-    id BIGINT,
-    student_number CHAR(12),
-    full_name TEXT,
-    student_email TEXT
-) AS $$
-DECLARE
-    v_student RECORD;
-BEGIN
-    SELECT s.id, s.student_number, s.full_name, s.student_email INTO v_student
-    FROM public.students s
-    JOIN public.face_embeddings f ON f.student_id = s.id
-    WHERE f.embedding <=> p_embedding < p_threshold
-    ORDER BY f.embedding <=> p_embedding
-    LIMIT 1;
-
-    IF FOUND THEN
-        INSERT INTO public.attendance_logs (student_id)
-        VALUES (v_student.id);
-
-        RETURN QUERY
-        SELECT 
-            v_student.id,
-            v_student.student_number,
-            v_student.full_name,
-            v_student.student_email;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
