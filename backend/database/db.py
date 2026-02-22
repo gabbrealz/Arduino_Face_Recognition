c3c877f80556dd383863a698faa984feb4485395
unknown <christianagot11@gmail.com>
2026-02-22 15:02:44 +0800

    made code cleaner and removed unnecessary methods in db.py

--- backend/database/db.py
+++ backend/database/db.py
@@ -17,34 +17,38 @@ class DB:
     def log_attendance_for_face(embedding, threshold):
         with DB.pool.connection() as conn:
             with conn.cursor() as cur:
-                return cur.execute("SELECT * FROM log_attendance_for_face(%s, %s)", (embedding, threshold)).fetchone()
+                cur.execute("SELECT * FROM log_attendance_for_face(%s, %s)", (embedding, threshold))
+                return cur.fetchone()
 
     @staticmethod
     def register_face(embedding):
         with DB.pool.connection() as conn:
             with conn.cursor() as cur:
-                return cur.execute("SELECT register_face(%s)", (embedding,)).fetchone()
+                cur.execute("SELECT register_face(%s)", (embedding,))
+                return cur.fetchone()["register_face"]
 
 
     @staticmethod
     def get_students():
         with DB.pool.connection() as conn:
             with conn.cursor() as cur:
-                return cur.execute("SELECT id, student_number, full_name, student_email FROM public.students").fetchall()
+                cur.execute("SELECT id, student_number, full_name, student_email FROM public.students")
+                return cur.fetchall()
 
     @staticmethod
     def get_student(student_id):
         with DB.pool.connection() as conn:
             with conn.cursor() as cur:
-                return cur.execute(
+                cur.execute(
                     "SELECT id, student_number, full_name, student_email FROM public.students WHERE id = %s LIMIT 1", 
-                    (student_id,)).fetchone()
+                    (student_id,))
+                return cur.fetchone()
 
     @staticmethod
     def insert_student(student_number, full_name, student_email):
         with DB.pool.connection() as conn:
             with conn.cursor() as cur:
-                return cur.execute(
+                cur.execute(
                     "INSERT INTO public.students (student_number, full_name, student_email) VALUES (%s, %s, %s)",
                     (student_number, full_name, student_email))
     
@@ -53,11 +57,12 @@ class DB:
     def get_attendance_logs():
         with DB.pool.connection() as conn:
             with conn.cursor() as cur:
-                return cur.execute("""
+                cur.execute("""
                     SELECT l.created_at, s.student_number, s.full_name, s.student_email
                     FROM public.attendance_logs l
                     JOIN public.students s ON l.student_id = s.id
-                """).fetchall()
+                """)
+                return cur.fetchall()
         
     @staticmethod
     def insert_attendance_log(student_id):
@@ -66,18 +71,3 @@ class DB:
                 cur.execute(
                     "INSERT INTO public.attendance_logs (student_id) VALUES (%s)",
                     (student_id,))
-
-
-    @staticmethod
-    def get_face_embeddings():
-        with DB.pool.connection() as conn:
-            with conn.cursor() as cur:
-                return cur.execute("SELECT * FROM public.face_embeddings").fetchall()
-
-    @staticmethod
-    def insert_face_embedding(student_id, embedding):
-        with DB.pool.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    "INSERT INTO public.face_embeddings (student_id, embedding) VALUES (%s, %s)",
-                    (student_id, embedding))
