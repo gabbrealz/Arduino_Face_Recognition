f404f1b04395a820839e9ada6bbaf180d2a35623
unknown <christianagot11@gmail.com>
2026-02-26 01:41:05 +0800

    storing the latest streamed image to app state

--- backend/routers/websocket.py
+++ backend/routers/websocket.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import Request, APIRouter, WebSocket
 from services.connection_manager import stream_manager, event_manager, microcontroller_manager
 
 router = APIRouter()
 
 @router.websocket("")
-async def streaming_endpoint(websocket: WebSocket):
+async def streaming_endpoint(request: Request, websocket: WebSocket):
     await websocket.accept()
     stream_manager.connect(websocket)
 
@@ -15,6 +15,7 @@ async def streaming_endpoint(websocket: WebSocket):
             data = message.get("bytes")
             if data is not None:
                 print("Received BYTES")
+                request.app.state.img = data
                 await stream_manager.broadcast_bytes(data, websocket)
 
     finally:
