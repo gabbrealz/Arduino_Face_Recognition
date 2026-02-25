e4d877656c129dd1a355971346846f2c9046266c
unknown <christianagot11@gmail.com>
2026-02-25 16:57:41 +0800

    removed 2 websocket endpoints (using mqtt instead)

--- backend/routers/websocket.py
+++ backend/routers/websocket.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, WebSocket
-from services.connection_manager import stream_manager, event_manager
+from services.connection_manager import stream_manager, event_manager, microcontroller_manager
 
 router = APIRouter()
 
 @router.websocket("")
-async def websocket_endpoint(websocket: WebSocket):
+async def streaming_endpoint(websocket: WebSocket):
     await websocket.accept()
     stream_manager.connect(websocket)
 
@@ -23,30 +23,8 @@ async def websocket_endpoint(websocket: WebSocket):
 
             data = message.get("bytes")
             if data is not None:
-                # print("Received BYTES")
+                print("Received BYTES")
                 await stream_manager.broadcast_bytes(data, websocket)
 
     finally:
         stream_manager.disconnect(websocket)
-
-
-@router.websocket("/event")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    event_manager.connect(websocket)
-
-    try:
-        while True:
-            message = await websocket.receive()
-
-            if message["type"] == "websocket.disconnect":
-                print("Received disconnect message")
-                break
-
-            text = message.get("text")
-            if text is not None:
-                print("Received TEXT")
-                await event_manager.broadcast(text, websocket)
-
-    finally:
-        event_manager.disconnect(websocket)
